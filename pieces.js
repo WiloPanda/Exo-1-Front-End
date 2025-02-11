579c7c2d60301332462d2ee3b6a197666f3d8cd5
WiloPanda <wilona.middendorp@gmail.com>
2025-02-12 00:12:20 +0100

    All products

--- pieces.js
+++ pieces.js
@@ -1,25 +1,79 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("pieces-autos.json");
+const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
-const dispoElement = document.createElement("p");
-dispoElement.innerText = article.disponible ? "En stock" : "Rupture de stock";
-// Ajout des éléments à la page
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(dispoElement);
+for (let i = 0; i < pieces.length; i++) {
+
+    const article = pieces[i];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+    // Création des balises 
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+    // On rattache la balise article a la section Fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache l’image à pieceElement (la balise article)
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+}
+
+// Gestion des boutons
+// Tri des pièces par prix croissant
+const boutonTrier = document.querySelector(".btn-trier");
+
+boutonTrier.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return a.prix - b.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+// Filtrage des pièces par prix abordable
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+
+boutonFiltrer.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+    console.log(piecesFiltrees);
+});
+
+//Tri des pièces par prix décroissant
+const boutonTrierDecroissant = document.querySelector(".btn-trier-dcs");
+
+boutonTrierDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+    });
+    console.log(piecesOrdonnees);
+});
+
+// Filtrage des pièces par description
+const boutonFiltrerDescription = document.querySelector(".btn-filtrer-description");
+
+boutonFiltrerDescription.addEventListener("click", function () {
+    const piecesFiltreesDescription = pieces.filter(function (piece) {
+        return piece.description;
+    });
+    console.log(piecesFiltreesDescription)
+});
+
