1a4fe5e095b7b253335e02ca51022ef022d9c4cf
WiloPanda <wilona.middendorp@gmail.com>
2025-02-13 09:36:03 +0100

    Abordable/dispo

--- pieces.js
+++ pieces.js
@@ -21,7 +21,7 @@ for (let i = 0; i < pieces.length; i++) {
     const descriptionElement = document.createElement("p");
     descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
     const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+    stockElement.innerText = article.disponible ? "En stock" : "Rupture de stock";
 
     // On rattache la balise article a la section Fiches
     sectionFiches.appendChild(pieceElement);
@@ -77,3 +77,32 @@ boutonFiltrerDescription.addEventListener("click", function () {
     console.log(piecesFiltreesDescription)
 });
 
+// Nom des pièces abordables
+const noms = pieces.filter(piece => piece.prix <= 35) // Garde seulement les éléments à prix <= 35
+    .map(piece => piece.nom); // Récupère uniquement les noms
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+document.querySelector('.abordables').appendChild(abordablesElements)
+//Ajout de chaque nom à la liste
+for (let i = 0; i < noms.length; i++) {
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+
+// Nom des pièces en stock
+const enStockNom = pieces.filter(piece => piece.disponible === true)
+    .map(piece => piece.nom);
+const enStockPrix = pieces.filter(piece => piece.disponible === true)
+    .map(piece => piece.prix);
+
+//Création de la liste
+const enStockElements = document.createElement('ul');
+document.querySelector('.disponibles').appendChild(enStockElements)
+//Ajout de chaque nom + prix à la liste
+for (let i = 0; i < enStockNom.length; i++) {
+    const nomElement = document.createElement('li');
+    nomElement.innerText = `${enStockNom[i]} - ${enStockPrix[i]} €`;
+    enStockElements.appendChild(nomElement)
+}
